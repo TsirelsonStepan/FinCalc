d0e68aa15c21b95ba21178f37667ee94c5d37a72
Stepan Tsirelson <tsirelsons@gmail.com>
2026-01-03 15:11:56 +0300

    Removed AlphaVantageAPI examples

--- AlphaVintageAPI/AlphaVantageAPI.cs
+++ AlphaVintageAPI/AlphaVantageAPI.cs
@@ -5,47 +5,40 @@ namespace FinCalc.AlphaVantageAPI
 	class AlphaVantageAPI
 	{
 		private static readonly string domain = "https://www.alphavantage.co/query?";
-		private static readonly string api_key = "your_api_key";
+		private static readonly string apiKey = "yourApiKey";
 
 		private static async Task<string> GETReporting(string ticker, string function)
 		{
 			HttpClient client = new();
-			string url = domain + function + $"&symbol={ticker}" + $"&apikey={api_key}";
-			//string response = await client.GetStringAsync(url);
+			string url = domain + function + $"&symbol={ticker}" + $"&apikey={apiKey}";
+			string response = await client.GetStringAsync(url);			
 			
-			
-			string response = "";
-			if (function == "function=CASH_FLOW") response = File.ReadAllText("./cash_flow_statement.json");
-			else if (function == "function=BALANCE_SHEET") response = File.ReadAllText("./balance_sheet.json");
-			else if (function == "function=INCOME_STATEMENT") response = File.ReadAllText("./income_statement_query.json");
-			
-			
-			string last_annual_report = GetLastPeriod(response);
-			return last_annual_report;
+			string lastAnnualReport = GetLastPeriod(response);
+			return lastAnnualReport;
 		}
 
 		public static string GETCashFlow(string ticker)
 		{
-			string cash_flow = GETReporting(ticker, "function=CASH_FLOW").Result;
-			return cash_flow;
+			string cashFlow = GETReporting(ticker, "function=CASH_FLOW").Result;
+			return cashFlow;
 		}
 		public static string GETBalanceSheet(string ticker)
 		{
-			string balance_sheet = GETReporting(ticker, "function=BALANCE_SHEET").Result;
-			return balance_sheet;
+			string balanceSheet = GETReporting(ticker, "function=BALANCE_SHEET").Result;
+			return balanceSheet;
 		}
 		public static string GETIncomeStatement(string ticker)
 		{
-			string income_statement = GETReporting(ticker, "function=INCOME_STATEMENT").Result;
-			return income_statement;
+			string incomeStatement = GETReporting(ticker, "function=INCOME_STATEMENT").Result;
+			return incomeStatement;
 		}
 		
 		private static string GetLastPeriod(string reporting)
 		{
 			JsonDocument json = JsonDocument.Parse(reporting);
-			JsonElement annual_periods = json.RootElement.GetProperty("annualReports");
-			string last_period = annual_periods[0].ToString();
-			return last_period;
+			JsonElement annualPeriods = json.RootElement.GetProperty("annualReports");
+			string lastPeriod = annualPeriods[0].ToString();
+			return lastPeriod;
 		}
 	}
 }
