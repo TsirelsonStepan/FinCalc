bd630d32439a6bb7e8d4690fdf1157cb9d6b9796
Stepan Tsirelson <tsirelsons@gmail.com>
2026-01-04 16:48:11 +0300

    Display of all assets on the raph

--- wwwroot/js/calculations.js
+++ wwwroot/js/calculations.js
@@ -1,58 +1,73 @@
 const calculateButton = document.getElementById("calculate-btn");
 
 async function calculatePortfolio() {
-    const response = await fetch("/calculatePortfolio", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(selectedAssets),
-    });
-    if (!response.ok) throw new Error(response.status);
-    const calculations = await response.json();
-    displayCalculations(calculations);
+	const response = await fetch("/calculatePortfolio", {
+		method: "POST",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify(selectedAssets),
+	});
+	if (!response.ok) throw new Error(response.status);
+	const calculations = await response.json();
+	displayCalculations(calculations);
 }
 
 function displayCalculations(calculations) {
-    const wAPR = ((calculations.weightedAveragePortfolioReturn - 1) * 100).toFixed(2) + "%";
-    const ePR = ((calculations.expectedPortfolioReturn - 1) * 100).toFixed(2) + "%";
-    const B = calculations.portfolioBeta;
+	const weightedAverageReturn = ((calculations.weightedAveragePortfolioReturn - 1) * 100).toFixed(2) + "%";
+	const expectedReturn = ((calculations.expectedPortfolioReturn - 1) * 100).toFixed(2) + "%";
+	const historicalDates = calculations.portfolioHistoricData[0].dates;
+	const historicalValues = calculations.portfolioHistoricData;
+	const beta = calculations.portfolioBeta;
 
-    //document.getElementById("details-panel-1").querySelector(".stats-list");
-    document.querySelector('.main').innerHTML = `
-        <div class="group" id="group-return-ratios" data-target="details-panel-return-ratios">
-            <div class="collapse-icon">▶</div>
-            <div class="group-title">Return ratios</div>
-        </div>
-        <div class="details-panel" id="details-panel-return-ratios">
-            <div class="stats-list">
-                <p>Weighted Average Portfolio Return: ${wAPR}</p>
-                <p>Expected Portfolio Return: ${ePR}</p>
-            </div>
-        </div>
-        
-        <div class="group" id="group-historical-data" data-target="details-panel-historical-data">
-            <div class="collapse-icon">▶</div>
-            <div class="group-title">Historical data</div>
-        </div>
-        <div class="details-panel" id="details-panel-historical-data">
-            <div class="graph-holder">
-                <canvas id="myChart"></canvas>
-            </div>
-        </div>`;
+	document.querySelector('.main').innerHTML = "";
+	createReturnRatiosGroup(weightedAverageReturn, expectedReturn);
+	createGraphGroup(historicalDates, historicalValues);
 
-    new Chart("myChart", {
-        type: "line",
-        data: {
-            labels: calculations.portfolioHistoricData[0].dates,
-            datasets: [{
-                label: "Portfolio value",
-                data: calculations.portfolioHistoricData[0].values
-            }]
-        }
-    });
+	Array.from(document.getElementsByClassName('group')).forEach(group => {
+		group.addEventListener('click', () => manageGroups(group));
+	});
+}
 
-    Array.from(document.getElementsByClassName('group')).forEach(group => {
-        group.addEventListener('click', () => manageGroups(group));
-    });
+function createGraphGroup(dates, values) {
+	const graphFill = `
+	<div class="group" id="group-historical-data" data-target="details-panel-historical-data">
+		<div class="collapse-icon">▶</div>
+		<div class="group-title">Historical data</div>
+	</div>
+	<div class="details-panel" id="details-panel-historical-data">
+		<div class="graph-holder">
+			<canvas id="myChart"></canvas>
+		</div>
+	</div>`;
+
+	document.querySelector('.main').innerHTML += graphFill;
+	const valuesData = [];
+	values.forEach(value => {
+		valuesData.push({label: value.name, data: [...value.values].reverse()});
+	});
+
+	new Chart("myChart", {
+		type: "line",
+		data: {
+			labels: [...dates].reverse(),
+			datasets: valuesData
+		}
+	});
+}
+
+function createReturnRatiosGroup(wAPR, ePR) {
+	const textFill = `        
+	<div class="group" id="group-return-ratios" data-target="details-panel-return-ratios">
+		<div class="collapse-icon">▶</div>
+		<div class="group-title">Return ratios</div>
+	</div>
+	<div class="details-panel" id="details-panel-return-ratios">
+		<div class="stats-list">
+			<p>Weighted Average Portfolio Return: ${wAPR}</p>
+			<p>Expected Portfolio Return: ${ePR}</p>
+		</div>
+	</div>`;
+
+	document.querySelector('.main').innerHTML += textFill;
 }
 
 calculateButton.addEventListener("click", calculatePortfolio);
